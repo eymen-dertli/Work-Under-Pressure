a2fa0d22ca31170471be9cc80916e8fe09e04fab
Eymen <eeymen200206@gmail.com>
2026-06-11 02:40:23 +0300

    düzenlemeler

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    private const string HtpButtonName = "HtpButton";
+    private const string HowToPlayPanelName = "HowToPlayPanel";
+
     public void PlayGame()
     {
         LevelProgression.TryLoadLevel(1);
@@ -10,6 +14,30 @@ public class MenuController : MonoBehaviour
 
     public GameObject mainMenuPanel;
     public GameObject levelsPanel;
+    public GameObject howToPlayPanel;
+
+    private void Awake()
+    {
+        if (howToPlayPanel == null)
+        {
+            howToPlayPanel = GameObject.Find(HowToPlayPanelName);
+        }
+
+        if (howToPlayPanel != null)
+        {
+            howToPlayPanel.SetActive(false);
+        }
+
+        GameObject htpButtonObject = GameObject.Find(HtpButtonName);
+        if (htpButtonObject != null && htpButtonObject.TryGetComponent(out Button htpButton))
+        {
+            if (htpButton.onClick.GetPersistentEventCount() == 0)
+            {
+                htpButton.onClick.RemoveListener(OpenHowToPlayPanel);
+                htpButton.onClick.AddListener(OpenHowToPlayPanel);
+            }
+        }
+    }
 
     public void OpenLevelsPanel()
     {
@@ -23,6 +51,32 @@ public class MenuController : MonoBehaviour
         }
 
         controller.Refresh();
+    }
+
+    public void OpenHowToPlayPanel()
+    {
+        if (levelsPanel != null)
+        {
+            levelsPanel.SetActive(false);
+        }
+
+        if (howToPlayPanel != null)
+        {
+            howToPlayPanel.SetActive(true);
+        }
+    }
+
+    public void CloseHowToPlayPanel()
+    {
+        if (howToPlayPanel != null)
+        {
+            howToPlayPanel.SetActive(false);
+        }
+
+        if (mainMenuPanel != null)
+        {
+            mainMenuPanel.SetActive(true);
+        }
     }
 
     public void ResetLevelProgress()
